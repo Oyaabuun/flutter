8d74512e5cf0becb4ba7982fdc2ce06c8d20b5f9
harit dey <deyharit@gmail.com>
2024-08-23 02:18:30 +0530

    first commit afdasfasf as f

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,6 +3,11 @@ import "package:flutter/material.dart";
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
+//Alignment? startAlignment; 
+//the above commented line can also be used for 
+//varaible declaration but it will show error in begin and end at the later portion of the code 
+//this method of variable declaration means we are telling dart
+//that our variable is alignmet data type and the '?' means that it can accept null type of values 
 
 class GradientContainer extends StatelessWidget{
    const GradientContainer({super.key});
@@ -18,7 +23,7 @@ class GradientContainer extends StatelessWidget{
                 Color.fromARGB(255, 6, 82, 3),
                 Color.fromARGB(255, 103, 160, 37),
               ],
-              begin: startAlignment, 
+              begin: startAlignment, //when you use Alignment? startAlignment; in variable declaraion in starting of code here on begin and end we will have errors since it doesnt want null values so better to use var method 
               end: endAlignment,
             ),
         
